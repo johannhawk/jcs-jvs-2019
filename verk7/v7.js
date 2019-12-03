7561dfd06fbdd7121bf1423677560a37051ddd56
johannhawk <johannsi223@nemi.tskoli.is>
2019-12-03 16:09:57 +0000

    Add files via upload

--- verk7/v7.js
+++ verk7/v7.js
@@ -6,36 +6,29 @@ var mainco = document.getElementById("main");
 //data url: http://apis.is/concerts
 
 function nameFilter(rData) {
-	find = document.getElementById('nameSearch').value;//tekur value ut ur search bar
-	sDate = document.getElementById('startDate').value;//tekur value ut ur date input 1
-	eDate = document.getElementById('endDate').value;//tekur value ut ur date input 2
+	find = document.getElementById('nameSearch').value.toLowerCase();//tekur value ut ur search bar, gerir það að lowercase
+	sDate = document.getElementById('startDate').value;//tekur value ut ur fyrra date input
+	eDate = document.getElementById('endDate').value;//tekur value ut ur seinna date input
 
-	console.log(sDate);
-	console.log(eDate);
-	//console.log(rData.results);
-	//console.log(find);
 	//String.prototype.includes() verdur notad
 	let rArray = [];
 	let newArray = [];
 	for (let i = 0; i < rData.results.length; i++) {
-		let bah = rData.results[i].eventDateName;
+		let bah = rData.results[i].eventDateName.toLowerCase();//tekur eventdate name og gerir það að lowercase til að vinna með .includes()
 
 		let year = rData.results[i].dateOfShow.substr(0,4);
 		let month = rData.results[i].dateOfShow.substr(5,2);
-		let day = rData.results[i].dateOfShow.substr(8,2);//taka efni sem er haegt ad nota
-		console.log(year);
-		console.log(month);
-		console.log(day);
-		if (bah.includes(find) || find == '') {
-			if (sDate.substr(0,4) =< ){
-				rArray[i] = rData.results[i];
+		let day = rData.results[i].dateOfShow.substr(8,2);//taka efni ut ur array sem er haegt ad nota og gera þau léttara að höndla
+		if (bah.includes(find) || find == '') {//search filter
+			if (sDate.substr(0,4) <= year && year <= eDate.substr(0,4)){//ára filter
+				if (sDate.substr(5,2) <= month && month <= eDate.substr(5,2)){//mánaðar filter
+					if(sDate.substr(8,2) <= day && day <= eDate.substr(8,2)){//daga filter
+						rArray[i] = rData.results[i];
+						}
+					}
+				}
 			}
-			
-		}
-	}
-
-
-	
+		}	
 	newArray = rArray.filter(function(){return true;});
 	//for loop i fet() function vil að telja upp frá 0 og ekki fyrsta tölunna sem filterið sem ég gerði gefur það svo ég nota annan filter til að laga það 
 	return newArray;
@@ -62,11 +55,12 @@ function fet() { fetch('https://apis.is/concerts')  //  JSON skrá sem eru local
       // Examine the text in the response
       response.json().then(function(data) {
       	mainco.innerHTML = "";//hreinsar HTMLið
+        /*
         console.log(data); // hráar uppllýsingar, ég get notið það til að bera saman það sem kemur á síðunna við hráa efnið til að sjá ef eitthvað fór úrskeiðis
         console.log(data.results);
         console.log(data.results[0].eventDateName);
         console.log(data.results[1].eventDateName); // úttak er strengur td: U2 Tribute Austurland að Glettingi		
-
+		*/
         let fdata = [];
 
         fdata = nameFilter(data);//filter
@@ -99,6 +93,8 @@ function fet() { fetch('https://apis.is/concerts')  //  JSON skrá sem eru local
 
 fet();//fetch function
 
-filt.innerHTML = "<center><input type=text id=nameSearch onkeyup=fet() placeholder='Case sensitive leit...'></center>"
-+ "<br><center><input type=date id=startDate value=2019-12-01 min=2019-12-01 max=2020-01-01> <input type=date id=endDate value=2019-12-31 min=2019-12-01 max=2020-01-01></center>";
+//html fyrir leitar filter og dagatals filter formin, hefur "onkeyup" og "onselect" til að kalla á fall
+filt.innerHTML = "<center><input type=text id=nameSearch onkeyup=fet() placeholder='Leita...'></center>"
++ "<br><center><input type=date id=startDate oninput=fet() value=2019-12-01 min=2019-12-01 max=2020-01-01>" 
++ "<input type=date id=endDate oninput=fet() value=2019-12-31 min=2019-12-01 max=2020-01-01></center>";
 
